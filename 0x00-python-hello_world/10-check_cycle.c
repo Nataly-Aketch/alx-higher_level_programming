fa542ebd4018d26fddd19eca47fad2f5e595de76
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-07 07:41:03 +0300

    Cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: linked list
+ * Return: 1 if there is 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+
+	while (head)
+	{
+		if (head->next == list)
+			return (1);
+		head = head->next;
+	}
+	return (0);
+}
