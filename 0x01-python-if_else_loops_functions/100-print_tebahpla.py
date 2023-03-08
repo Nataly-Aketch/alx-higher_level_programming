cf31f0deef83b697196f3f5e76f43b366964de51
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-08 20:07:01 +0300

    Reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for w in range(122, 96, -1):
-    if w % 2 != 0:
-        print(chr(w - 32), end="")
-    else:
-        print(chr(w), end="")
+    print(chr(w - 32) if w % 2 != 0 else chr(w), end="")
