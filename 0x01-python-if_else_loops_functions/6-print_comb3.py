0041088096a5a935de8634e8ba1e6ed0cbd157ab
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-08 16:17:40 +0300

    Combo two

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(1, 10):
-        if i != j and j > i:
-            print("{}{}".format(i, j), end=" ")
-print(end="\n")
+        if i != j and j > i and i != 8:
+            print("{}{}".format(i, j), end=", ")
+        if j == 9 and i == 8:
+            print("{}{}".format(i, j))
