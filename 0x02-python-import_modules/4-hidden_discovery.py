98bea0eda1032bf2cc6cbdcc74facc733dd15834
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-09 22:50:53 +0300

    dir()

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-for w in dir(hidden_4):
-    if w[0] != '_':
-        print(w)
+if __name__ == "__main__":
+    for w in dir(hidden_4):
+        if w[0] != '_':
+            print(w)
