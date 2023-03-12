598d8803329de1f8bfd2638dd1eb7e20eeb6abb3
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-12 15:59:23 +0300

    No c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     len1 = len(my_string)
-    new_string = my_string[:]
+    new_string = ''
     for i in range(len1):
         if my_string[i] == 'c' or my_string[i] == 'C':
             new_string = my_string[:i] + my_string[i + 1:]
