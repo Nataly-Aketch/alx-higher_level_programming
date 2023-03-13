9171c07effba309fd259261ede51bc62fe4e160e
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-13 11:11:34 +0300

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,5 +9,4 @@ def print_matrix_integer(matrix=[[]]):
                 else:
                     print("{:d}".format(row[i]), end="\n")
     else:
-        print()
-        return
+        print("")
