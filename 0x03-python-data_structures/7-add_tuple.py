e89ac79a6aebf2e731c34a452b93d2f9ade374e3
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-13 09:31:07 +0300

    Add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+    if len1 < 2:
+        tuple_a = tuple_a + (0, 0)
+    if len2 < 2:
+        tuple_b = tuple_b + (0, 0)
+    x = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+    return (x, y)
