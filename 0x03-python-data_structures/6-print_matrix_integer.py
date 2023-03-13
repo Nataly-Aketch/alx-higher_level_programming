e13552e23180f7063c85773868aa4d8630859c86
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-13 11:18:01 +0300

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    len1 = len(matrix)
-    if len1 > 1:
+    len1 = len(matrix) - 1
+    if len1 > 0:
         for row in matrix:
             for i in range(len(row)):
                 if i != len(row) - 1:
