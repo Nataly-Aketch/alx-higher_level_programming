9cf2e7a6f86c9aa8e91f5b10ba3aacb727edb7cb
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-15 23:41:43 +0300

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    list1 = []
+    if a_dictionary is None:
+        return None
+    for k, v in a_dictionary.items():
+        val = a_dictionary.get(k)
+        list1 += [val]
+    large = list1[0]
+    for i in list1:
+        if i > large:
+            large = i
+    for k, v in a_dictionary.items():
+        val1 = a_dictionary.get(k)
+        if val1 == large:
+            n = k
+    return n
