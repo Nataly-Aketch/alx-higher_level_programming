d4b3c1f19017049d5c22a1853647e304fb21d5ca
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-15 15:58:43 +0300

    Replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,4 @@
 def search_replace(my_list, search, replace):
     new_list = []
     new_list = list(map(lambda i: replace if i == search else i, my_list))
-    '''for i in my_list:
-        if i == search:
-            i = replace
-        new_list += [i]'''
     return new_list
