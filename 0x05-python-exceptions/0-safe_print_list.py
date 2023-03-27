3bccd0212b2930f2b4acc216775b623ca3249b3a
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-27 12:24:09 +0300

    Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        count2 = 0
+        for i in my_list:
+            count += 1
+        if x >= count:
+            raise IndexError
+    except IndexError:
+        for j in range(count):
+            print(my_list[j], end="")
+            count2 += 1
+        print(end="\n")
+    else:
+        for j in range(x):
+            print(my_list[j], end="")
+            count2 += 1
+        print(end="\n")
+    return count2
