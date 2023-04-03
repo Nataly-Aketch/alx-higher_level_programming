7ef831928225d4fda2b647af8e32bcd9003f249b
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-03 21:46:28 +0300

    __repr__

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
             + "#" * self.__width
 
     def __repr__(self):
-        return "Rectangle(" + repr(self.width) + "," + repr(self.height) + ")"
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
