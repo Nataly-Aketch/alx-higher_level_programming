25f28dd81dc3733fcb6edd85ae6d9065f47a2de7
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-05 15:15:41 +0300

    Matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,21 +9,26 @@ def matrix_divided(matrix, div):
         div - divisor
     returns a new matrix
     Raises exceptions"""
+    err = "matrix must be a matrix (list of lists) of integers/floats"
     if div == 0:
-        raise ZeroDivisionError("division by 0")
+        raise ZeroDivisionError("division by zero")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if not all(isinstance(item, list) for item in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err)
     if len(set(map(len, matrix))) != 1:
         raise TypeError("Each row of the matrix must have the same size")
     new = []
     for row in matrix:
         for i in range(len(row)):
             if not isinstance(row[i], (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(err)
             result = row[i] / div
             new.append(round(result, 2))
     len1 = len(row)
-    res = [new[0:len1]] + [new[len1:]]
+    res = []
+    if len1 != 1:
+        res = [new[0:len1]] + [new[len1:]]
+    else:
+        res += [new]
     return res
