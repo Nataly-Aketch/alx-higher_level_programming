742801f60f8e8b3ff798cac8da2556058f37e653
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-11 20:22:19 +0300

    Advanced task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,6 +8,7 @@ class MyInt(int):
     def __eq__(self, obj):
         """returns false if values are equal"""
         return False
+
     def __ne__(self, obj):
         """returns true if values are not equal"""
         return True
