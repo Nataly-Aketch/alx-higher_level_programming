8e7510d84f2b5ea8996a1182362225a8056e800b
Nataly-Aketch <nataliebeckie@gmail.com>
2023-05-17 22:12:19 +0300

    Count columns

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Each record displays: <TV Show genre> - <Number of shows linked to this genre>
+-- First column is called genre
+-- Second column is called number_of_shows
+-- Doesn’t display a genre that wiyhout any shows linked
+-- Results are sorted in descending order by the number of shows linked
+SELECT name as genre, COUNT(*) as number_of_shows
+FROM tv_genres tg
+INNER JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
+GROUP BY tsg.genre_id
+ORDER BY number_of_shows DESC;
