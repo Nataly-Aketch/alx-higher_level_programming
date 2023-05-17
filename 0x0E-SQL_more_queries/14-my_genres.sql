8df11a4d5e3b08cceceb48a3dc7b89d653fbccfb
Nataly-Aketch <nataliebeckie@gmail.com>
2023-05-17 22:38:49 +0300

    Multiple joins

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,11 +2,11 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record displays: tv_genres.name
 -- Results are sorted in ascending order by the genre name
-SELECT name AS genre
+SELECT name
 FROM tv_genres tg
 INNER JOIN tv_show_genres tsg 
 ON tg.id = tsg.genre_id 
 INNER JOIN tv_shows ts
-ON ts.id = tsg.show_id 
+ON ts.id = tsg.show_id
 WHERE ts.title = 'Dexter'
 ORDER BY tg.name ASC;
